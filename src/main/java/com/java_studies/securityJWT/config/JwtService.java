8ba37b14bf6b67951ee70d5e3a8e0d10b137c391
leonardorafaelli <leonardorafaelli@gmail.com>
2024-05-03 21:29:08 +0200

    feat: extract jwt claims and subject

--- src/main/java/com/java_studies/securityJWT/config/JwtService.java
+++ src/main/java/com/java_studies/securityJWT/config/JwtService.java
@@ -1,11 +1,62 @@
 package com.java_studies.securityJWT.config;
 
+import com.java_studies.securityJWT.user.UserDtls;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.security.PublicKey;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
+    
+    private static final String SECRET_KEY = "DBDDA5EAF88E8E38E47EA728A2F7B";
 
     public String extractUsername(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
     }
+    
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDtls userDtls
+    ){
+        return Jwts
+                .builder()
+                .claims(extraClaims)
+                .subject(userDtls.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey())
+                .compact();
+    }
+
+    private Claims extractAllClaims(String token){
+      return Jwts
+              .parser()
+              .verifyWith(getSignInKey())
+              .build()
+              .parseSignedClaims(token)
+              .getPayload();
+    }
+
+    private SecretKey getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    };
 }
