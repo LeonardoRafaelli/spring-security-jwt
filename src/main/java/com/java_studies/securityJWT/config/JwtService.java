de6774802b17b369a6c1a5f8c3d564c2b0509436
leonardorafaelli <leonardorafaelli@gmail.com>
2024-05-04 16:43:47 +0200

    feat: adds jwtService claims handles and token verifications

--- src/main/java/com/java_studies/securityJWT/config/JwtService.java
+++ src/main/java/com/java_studies/securityJWT/config/JwtService.java
@@ -15,6 +15,7 @@ import javax.crypto.SecretKey;
 import java.security.Key;
 import java.security.PublicKey;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -32,6 +33,10 @@ public class JwtService {
         return claimsResolver.apply(claims);
     }
 
+    public String generateToken(UserDtls userDtls) {
+      return generateToken(new HashMap<>(), userDtls);
+    };
+
     public String generateToken(
             Map<String, Object> extraClaims,
             UserDtls userDtls
@@ -44,6 +49,19 @@ public class JwtService {
                 .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
                 .signWith(getSignInKey())
                 .compact();
+    }
+
+    public boolean isTokenValid(String token, UserDtls userDtls) {
+        final String username = extractUsername(token);
+        return username.equals(userDtls.getUsername()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 
     private Claims extractAllClaims(String token){
